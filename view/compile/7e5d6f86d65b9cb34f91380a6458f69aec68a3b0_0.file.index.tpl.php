1a94a85fa4effde2d229ca282be980e8dc93e074
Marcia <marciasprates@hotmail.com>
2020-12-20 17:44:31 -0300

    Meu primeiro commit

---
+++ view/compile/7e5d6f86d65b9cb34f91380a6458f69aec68a3b0_0.file.index.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.36, created on 2020-12-18 00:48:58
+  from 'C:\xampp\htdocs\MShop\view\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.36',
+  'unifunc' => 'content_5fdbee6a7cb724_03175255',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7e5d6f86d65b9cb34f91380a6458f69aec68a3b0' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\MShop\\view\\index.tpl',
+      1 => 1608248875,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5fdbee6a7cb724_03175255 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html>
+<head>
+	<title>Loja Virtual</title>
+</head>
+<body>
+	<h1>Essa é a página inicial</h1>
+	<h2><?php echo $_smarty_tpl->tpl_vars['NOME']->value;?>
+</h2>
+</body>
+</html><?php }
+}
---
+++ view/index.tpl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loja Virtual</title>
+</head>
+<body>
+	<h1>Essa é a página inicial</h1>
+	<h2>{$NOME}</h2>
+</body>
+</html>
